23cc5c32cadec7cb3645136a0244fb66b29e8b8a
kmfoysal <faisal.cri@gmail.com>
2021-11-13 14:26:30 +0600

    create post api for create user,put api for upsart google login and delete api for orders

--- index.js
+++ index.js
@@ -35,11 +35,48 @@ async function run (){
             res.send(products);
         })
 
+         // GET API For Orders
+         app.get('/orders', async(req, res)=>{
+          const email = req.query.email;
+          const query = {email: email} 
+          const cursor = ordersCollection.find(query);
+          const orders = await cursor.toArray();
+          res.send(orders);
+      })
+
+
         // Post API for Orders 
         app.post('/orders', async(req, res)=>{
           const order = req.body;
           const result = await ordersCollection.insertOne(order);
           res.json(result)
+      })
+
+      // Post api for create user
+      app.post('/users', async(req, res)=>{
+        const user = req.body;
+        const result = await usersCollection.insertOne(user);
+          res.json(result)
+      })
+
+
+      // Put api for upsart google login
+      app.put('/users', async(req, res)=>{
+        const user = req.body;
+        const filter = {email: user.email};
+        const options = {upsert:true};
+        const updateUser = {$set: user};
+        const result = await usersCollection.updateOne(filter, updateUser, options)
+        res.json(result);
+      })
+      
+
+       // DELETE API Orders
+       app.delete('/orders/:id', async(req, res)=>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result = await ordersCollection.deleteOne(query);
+        res.json(result);
       })
 
 
